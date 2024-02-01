edfec0b96450c632b2fbee291f0053b24cbc7532
NieamulKabir <kabir000079@gmail.com>
2024-02-01 12:15:33 +0600

    details update

--- src/pages/ShowDetails/ShowDetails.jsx
+++ src/pages/ShowDetails/ShowDetails.jsx
@@ -32,6 +32,7 @@ const ShowDetails = () => {
     rating,
   } = show;
 
+  // booking functionality
   const handleBook = (event) => {
     event.preventDefault();
     const form = event.target;
@@ -55,120 +56,130 @@ const ShowDetails = () => {
     : "";
 
   return (
-    <div className="mt-16 w-[90%] mx-auto pt-20 text-black lg:flex">
-      <div className="hero min-h-screen bg-gray-300 rounded-xl">
-        <div className="hero-content flex-col lg:flex-row">
-          <img src={image?.medium} className="max-w-sm rounded-lg shadow-2xl" />
-          <div>
-            <p className="">
-              <b className="text-lg">{name}</b> {summary}
-            </p>
-            <form onSubmit={handleBook}>
-              <div className="card-actions justify-end">
-                {/* The button to open modal */}
+    <div>
+      <h1 className="mt-16 w-[90%] mx-auto py-6 text-4xl font-bold">Show Details - <span className="text-[#38b6aa]">{name}</span></h1>
+      <div className="w-[90%] mx-auto  text-black lg:flex">
+        <div className="hero min-h-screen bg-gray-300 rounded-xl">
+          <div className="hero-content flex-col lg:flex-row">
+            <img
+              src={image?.medium}
+              className="max-w-sm rounded-lg shadow-2xl"
+            />
+            <div>
+              <p className="">
+                <b className="text-lg">{name}</b> {summary}
+              </p>
+              {/* booking ticket form using a modal  */}
+              <form onSubmit={handleBook}>
+                <div className="card-actions justify-end">
+                  {/* The button to open modal */}
 
-                {/* Put this part before </body> tag */}
-                <input
-                  type="checkbox"
-                  id="my-modal-6"
-                  className="modal-toggle"
-                />
-                <div className="modal modal-bottom sm:modal-middle text-black">
-                  <div className="modal-box relative md:mr-44 bg-gray-800 text-black">
-                    <input
-                      name="name"
-                      type="text"
-                      defaultValue={name}
-                      placeholder="Movie Name"
-                      className="input w-full input-bordered my-1 text-black font-semibold"
-                      required
-                    />
-                    <input
-                      name="schedule"
-                      type="schedule"
-                      defaultValue={defaultSchedule}
-                      placeholder="schedule"
-                      className="input w-full input-bordered my-1 "
-                    />
-                    <br />
-                    <input
-                      name="rating"
-                      type="rating"
-                      defaultValue={rating?.average}
-                      placeholder="rating"
-                      className="input w-full input-bordered my-1 "
-                    />
-                    <br />
-                    <input
-                      name="status"
-                      type="status"
-                      defaultValue={status}
-                      placeholder="status"
-                      className="input w-full input-bordered my-1 "
-                    />
-                    <br />
-                    <input
-                      className="btn w-[40%] absolute right-[30%] mt-4"
-                      type="submit"
-                      value="Submit"
-                    />{" "}
-                    <br />
-                    <div className="modal-action mb-4">
-                      <label
-                        htmlFor="my-modal-6"
-                        className="btn btn-sm btn-circle absolute right-2 top-2 "
-                      >
-                        ✕
-                      </label>
+                  {/* Put this part before </body> tag */}
+                  <input
+                    type="checkbox"
+                    id="my-modal-6"
+                    className="modal-toggle"
+                  />
+                  <div className="modal modal-bottom sm:modal-middle text-black">
+                    <div className="modal-box relative md:mr-44 bg-gray-800 text-black">
+                      <input
+                        name="name"
+                        type="text"
+                        defaultValue={name}
+                        placeholder="Movie Name"
+                        className="input w-full input-bordered my-1 text-black font-semibold"
+                        required
+                      />
+                      <input
+                        name="schedule"
+                        type="schedule"
+                        defaultValue={defaultSchedule}
+                        placeholder="schedule"
+                        className="input w-full input-bordered my-1 "
+                      />
+                      <br />
+                      <input
+                        name="rating"
+                        type="rating"
+                        defaultValue={rating?.average}
+                        placeholder="rating"
+                        className="input w-full input-bordered my-1 "
+                      />
+                      <br />
+                      <input
+                        name="status"
+                        type="status"
+                        defaultValue={status}
+                        placeholder="status"
+                        className="input w-full input-bordered my-1 "
+                      />
+                      <br />
+                      <input
+                        className="btn w-[40%] absolute right-[30%] mt-4"
+                        type="submit"
+                        value="Submit"
+                      />{" "}
+                      <br />
+                      <div className="modal-action mb-4">
+                        <label
+                          htmlFor="my-modal-6"
+                          className="btn btn-sm btn-circle absolute right-2 top-2 "
+                        >
+                          ✕
+                        </label>
+                      </div>
                     </div>
                   </div>
+                  <div className="mb-4 mr-4 ">
+                    <label
+                      htmlFor="my-modal-6"
+                      className="btn bg-gray-800 hover:bg-gray-600 text-white flex  mt-10"
+                    >
+                      Book Ticket
+                    </label>
+                  </div>
                 </div>
-                <div className="mb-4 mr-4">
-                  <label htmlFor="my-modal-6" className="btn btn-primary">
-                    Book Ticket
-                  </label>
-                </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div>
-        {/* show information  */}
-        <div className="card mt-3 ml-3 bg-gray-300 text-black">
-          <div className="card-body px-4">
-            <h2 className="text-left text-xl">Show Info,</h2>
-            <div>
-              <p>
-                <b className=""> Network:</b>
-                <span className="text-[#33b0a4] mr-1"> {network?.name}.</span>(
-                {premiered?.slice(0, 4)}-{ended?.slice(0, 4)})
-              </p>
-              <p>
-                <b>Schedule:</b>
-                {schedule?.days[0]} at {schedule?.time} ({runtime}mins)
-              </p>
-              <p>
-                <b>Status:</b>
-                {status}{" "}
-              </p>
-              <p>
-                <b>Show Type:</b>
-                {type}{" "}
-              </p>
-              <p>
-                <b>Genres:</b>
-                {genres}{" "}
-              </p>
-              <p>
-                <b className="pr-3">Official-site:</b>
-                {officialSite}{" "}
-              </p>
-              <p>
-                <b>Ratting:</b>
-                {rating?.average}
-              </p>
+        <div>
+          {/* show information  */}
+          <div className="card mt-3 ml-3 bg-gray-300 text-black">
+            <div className="card-body px-4">
+              <h2 className="text-left text-xl">Show Info,</h2>
+              <div>
+                <p>
+                  <b className=""> Network:</b>
+                  <span className="text-[#33b0a4] mr-1"> {network?.name}.</span>
+                  ({premiered?.slice(0, 4)}-{ended?.slice(0, 4)})
+                </p>
+                <p>
+                  <b>Schedule:</b>
+                  {schedule?.days[0]} at {schedule?.time} ({runtime}mins)
+                </p>
+                <p>
+                  <b>Status:</b>
+                  {status}{" "}
+                </p>
+                <p>
+                  <b>Show Type:</b>
+                  {type}{" "}
+                </p>
+                <p>
+                  <b>Genres:</b>
+                  {genres}{" "}
+                </p>
+                <p>
+                  <b className="pr-3">Official-site:</b>
+                  {officialSite}{" "}
+                </p>
+                <p>
+                  <b>Ratting:</b>
+                  {rating?.average}
+                </p>
+              </div>
             </div>
           </div>
         </div>
